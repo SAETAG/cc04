a21d72fc5b0ebe666ba87ffa0c6784d4bc6ae9b5
SAETAG <saeko.96@gmail.com>
2025-03-18 10:46:35 +0000

    変更

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,9 @@ import type { Metadata } from "next"
 import "./globals.css"
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.dev",
+  title: "Closet Chronicle",
+  description: "Closet Chronicle is the story of you and your closet.",
+  generator: "cc01",
 }
 
 export default function RootLayout({
